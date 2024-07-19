5cee7725f68595f2e7ddd21173f90d7e704012b6
Jeslin Marium Alex <jeslinmariumalex@gmail.com>
2024-07-19 11:49:55 +0530

    Uploading Files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import {orginals,action,trending,comedy,horror,romance,documentaries} from './urls'
+import NavBar from "./Components/NavBar/NavBar";
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Banner />
+      <RowPost url={orginals} title="Netflix Orginals" />
+      <RowPost url={action} title="Action" isSmall/>
+      <RowPost url={trending} title="Trending" isSmall/>
+      <RowPost url={comedy} title="Comedy" isSmall/>
+      <RowPost url={horror} title="Horror" isSmall/>
+      <RowPost url={romance} title="Romance" isSmall/>
+      {/* <RowPost url={documentaries} title="Documentaries" isSmall/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY ='158b82fb82837a26a986afe9c7217c87';
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
